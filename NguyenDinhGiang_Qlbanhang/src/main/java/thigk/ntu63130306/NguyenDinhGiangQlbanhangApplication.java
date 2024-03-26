c11244d63ef9479ed36ce8ee556f1664cd5bb4c5
giangboss2 <138667102+giangboss2@users.noreply.github.com>
2024-03-26 15:11:25 +0700

    sds
    sds

---
+++ NguyenDinhGiang_Qlbanhang/src/main/java/thigk/ntu63130306/NguyenDinhGiangQlbanhangApplication.java
@@ -0,0 +1,13 @@
+package thigk.ntu63130306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenDinhGiangQlbanhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenDinhGiangQlbanhangApplication.class, args);
+	}
+
+}
---
+++ NguyenDinhGiang_Qlbanhang/src/test/java/thigk/ntu63130306/NguyenDinhGiangQlbanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk.ntu63130306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenDinhGiangQlbanhangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
