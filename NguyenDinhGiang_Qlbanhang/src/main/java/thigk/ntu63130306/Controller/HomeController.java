a65ffa4b5faec30cfb9a58952ba2e9f4a5c3d542
giangboss2 <138667102+giangboss2@users.noreply.github.com>
2024-03-26 16:11:26 +0700

    sds
    sd

--- NguyenDinhGiang_Qlbanhang/src/main/java/thigk/ntu63130306/Controller/HomeController.java
+++ NguyenDinhGiang_Qlbanhang/src/main/java/thigk/ntu63130306/Controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import thigk.ntu63130306.Models.Product;
 import thigk.ntu63130306.Service.ProductService;
 
 @Controller
@@ -15,6 +18,24 @@ public class HomeController {
     @GetMapping({"/", "/home"})
     public String index(Model model) {
         model.addAttribute("products", productService.findAll());
-        return "Index";
+        return "index"; // Đảm bảo tên view là chính xác và case-sensitive
+    }
+
+    @GetMapping("/timkiem")
+    public String searchProduct(@RequestParam String code, Model model) {
+        Product product = productService.findByCode(code);
+        model.addAttribute("product", product);
+        return "searchResult"; // Đảm bảo bạn có một view tên là searchResult.html
+    }
+
+    @PostMapping("/them")
+    public String addProduct(Product product) {
+        productService.addProduct(product);
+        return "addProduct";
+    }
+    @PostMapping("/hienthi")
+    public String showHomePage(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "show";
     }
 }
