863a31e465058e364640e102b90d225624619be8
giangboss2 <138667102+giangboss2@users.noreply.github.com>
2024-05-21 00:46:29 +0700

    đ
    đ

---
+++ DuAnCuoiKi/cinema_back_end-master/cinema_back_end-master/src/main/java/com/example/cinema_back_end/dtos/RoomDTO.java
@@ -0,0 +1,12 @@
+package com.example.cinema_back_end.dtos;
+import lombok.Data;
+@Data
+public class RoomDTO {
+    private int id;
+    private String name;
+    private int capacity;
+    private double totalArea;
+    private String imgURL;
+    private BranchDTO branch;
+
+}
